3378038f94e1aac841c68bd3f27355e177f155e4
Thyrus Gorges <me@thyrusgorges.com>
2023-04-12 15:32:02 -0700

    Add OutputCommand module
    The output command module is simply a wrapper around the BuildCommand module for ergonomic purposes.

--- output_command.rb
+++ output_command.rb
@@ -1,2 +1,12 @@
-class OutputCommand
+require_relative './build_command'
+
+module OutputCommand
+
+  def self.call(directory:)
+    if directory.nil?
+      puts "Please pass a directory path for the output files"
+      exit 1
+    end
+    BuildCommand.call(File.realpath(directory))
+  end
 end
