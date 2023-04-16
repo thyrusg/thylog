0bff91555cc4902e0c1fef83202771a04b624a66
Thyrus Gorges <1009223+thyrusg@users.noreply.github.com>
2023-04-15 17:46:13 -0700

    Implemented HTML generator for posts index page

---
+++ input/all_posts.rb
@@ -0,0 +1,36 @@
+require 'erb'
+
+class PostsHTMLGenerator
+
+  attr_reader :template
+
+  def initialize(binding)
+    @binding = binding
+    @template = %q{
+
+    <html>
+      <head>
+        <title> All Posts </title>
+      </head>
+
+      <body>
+        <h1> Posts </h1>
+        <ul>
+          <% posts.each do |post| %>
+            <% name = post[:name] %>
+            <% filename = post[:filename] %>
+            <li> <a href="<%= filename %>"> <%= name %>  </a> </li>
+          <% end %>
+        </ul>
+      <body>
+
+    </html>
+    }
+    @rhtml = ERB.new(@template)
+  end
+
+  def run
+    @rhtml.result(@binding)
+  end
+
+end
