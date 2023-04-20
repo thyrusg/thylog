9486a7ba71eb879ef556d49d19e7c3f11a83522d
Thyrus Gorges <1009223+thyrusg@users.noreply.github.com>
2023-04-20 11:15:05 -0700

    Move YAML and Markdown extractor to Utisl

--- utils.rb
+++ utils.rb
@@ -7,3 +7,16 @@ module Utils
     filename[-2..-1] = "html"
     filename
   end
+
+  def self.extract_yaml_and_contents(path_to_file)
+    yaml_string = StringIO.new
+    file = File.open(file_path, "r")
+    until $_ == ""
+      yaml_string.puts(file.gets(chomp: true))
+    end
+    content = file.read
+    file.close
+    return YAML.safe_load(yaml_string.string), content
+  end
+
+end
